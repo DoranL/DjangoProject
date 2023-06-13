40e5b5fe003ff3d40077a13bbfd1dc682dd0d7b6
DoranL <ljhstarting414@gmail.com>
2023-06-13 17:18:41 +0900

    mu thanks

--- GameData.py
+++ GameData.py
@@ -1,7 +1,4 @@
 import json
-import time
-
-import requests
 import os
 import django
 
@@ -26,52 +23,9 @@ with connection.cursor() as cursor:
     cursor.execute("DELETE FROM rank")
     cursor.execute("DELETE FROM app")
 
-
 # 정보 테이블 생성
-def add_table(key):
-    url = f"https://store.steampowered.com/api/appdetails?appids={key}&l=korean"  # JSON 파일의 URL
-    response = requests.get(url)  # GET 요청 보내기
-    if response.status_code == 200:  # 요청이 성공한 경우
-        data = response.json()  # JSON 데이터 파싱
-        # 데이터 처리 작업 수행
-        try:
-            if data[str(key)]['data']['type'] == "game":
-                if data[str(key)]['data']['is_free'] == "True":
-                    App.objects.create(
-                        id=key,
-                        name=data[str(key)]['data']['name'],
-                        header_image=data[str(key)]['data']['header_image'],
-                        capsule_image=data[str(key)]['data']['capsule_image'],
-                        is_free=data[str(key)]['data']['is_free'],
-                    )
-                else:
-                    App.objects.create(
-                        id=key,
-                        name=data[str(key)]['data']['name'],
-                        header_image=data[str(key)]['data']['header_image'],
-                        capsule_image=data[str(key)]['data']['capsule_image'],
-                        is_free=data[str(key)]['data']['is_free'],
-                        initial_price=str(data[str(key)]['data']['price_overview']['initial'])[:-2],
-                        final_price=str(data[str(key)]['data']['price_overview']['final'])[:-2],
-                        discount_percent=data[str(key)]['data']['price_overview']['discount_percent'],
-                    )
-                print(f"{key} 추가 완료")
-            else:
-                raise Exception("'타입'(이)가 '게임'(이)가 아님")
-        except Exception as e:
-            print(f"{key} 추가 실패:", e)
-        return True
-    else:
-        print(f"{key} 요청 실패:", response.status_code)
-        return False
-
-
-# 요청을 실패했을 경우, 다시 반복
-for app in names_data:
-    while True:
-        if add_table(app['appid']):
-            break
-        time.sleep(300)
+objects = [App(id=data['appid'], name=data['name']) for data in names_data]
+App.objects.bulk_create(objects)
 
 # 순위 테이블 생성
 for data in ranks_data:
@@ -82,4 +36,4 @@ for data in ranks_data:
         peak_in_game=data['peak_in_game']
     )
 
-print('Data Saved')
+print('-----Data Saved')
